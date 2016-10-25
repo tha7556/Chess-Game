54a5ac1b288de0385f2eaa9df74b7fb4c1c93029
Tyler <tha7556@uncw.edu>
2016-10-25 17:24:18 -0400

    Calls printBoard()

--- src/main.lua
+++ src/main.lua
@@ -5,7 +5,5 @@ local game = Game:new({}) --Variable for the game
 game:setupGame()
 moves = game.board[2][1]:possibleMoves(game)
 moves = game.board[2][1]:checkMoves(game, moves)
---print(game.board[2][1].placeHolder.." has "..#moves.." moves")
-for i=1, #moves do
-	print(moves[i][1]..","..moves[i][2])
-end
+
+game:printBoard()
